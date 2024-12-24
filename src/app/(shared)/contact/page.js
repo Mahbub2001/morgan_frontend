feccee343508ffd3d5f06eec875d52fe2b68f7ed
Mahbub2001 <imahbubturza@gmail.com>
2024-12-24 18:25:19 +0600

    contact_update

--- src/app/(shared)/contact/page.js
+++ src/app/(shared)/contact/page.js
@@ -2,50 +2,82 @@ import React from "react";
 
 function Contact() {
   return (
-    <div className="container mx-auto px-6 py-12 max-w-screen-lg">
-      <div className="flex flex-col md:flex-row items-center justify-between gap-12">
-        <div className="w-full md:w-1/2">
-          <img
-            className="w-full h-[25rem] rounded-sm shadow-lg"
-            src="/images/c1.jpg"
-            alt="Contact Us"
-          />
-        </div>
-        <div className="w-full md:w-1/2 text-center md:text-left">
-          <h3 className="text-3xl font-bold mb-4">Contact Us</h3>
-          <p className="font-sans font-light text-sm md:text-base leading-relaxed">
-            You are welcome to contact us by phone, chat, or email. 
-            We strive to respond to inquiries within 2 days. Our team is here to 
-            assist you with any questions or concerns you may have.
+    <section className="body-font relative ">
+      <div className="container mx-auto px-5 py-24">
+        <div className="mb-12 flex w-full flex-col text-center text-xs text-gray-700">
+          <h1 className="title-font mb-4 text-2xl font-medium  sm:text-3xl tracking-widest">
+            CONTACT
+          </h1>
+          <p className="mx-auto leading-relaxed lg:w-2/3 my-6">
+            Please fill out the form below or email at info@gmail.com
+          </p>
+          <p>
+            Studio Telephone:16546 565 566{" "}
+            <span className="italic">Office Hours Only</span>{" "}
           </p>
         </div>
-      </div>
 
-      <div className="flex flex-col md:flex-row items-center justify-between gap-12 mt-16">
-        <div className="w-full md:w-1/2">
-        <h3 className="text-2xl font-bold">Contact Information</h3>
-          {[1, 2, 3].map((_, index) => (
-            <div key={index} className="mt-8">
-              <p className="font-sans font-light text-sm">Karina Mundt Holst</p>
-              <p className="font-sans font-light text-sm">
-                Owner and Creative Director
-              </p>
-              <p className="font-sans font-light text-sm">
-                kmh@decadentcopenhagen.dk
-              </p>
+        <div className="mx-auto md:w-2/3 lg:w-1/2">
+          <div className="-m-2 flex flex-wrap">
+            <div className="w-1/2 p-2">
+              <div className="relative">
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="peer w-full rounded border border-gray-700  bg-opacity-40 py-1 px-3 text-base leading-8 text-black placeholder-transparent outline-none transition-colors duration-200 ease-in-out focus:border-none focus:ring-2 focus:ring-indigo-900"
+                  placeholder="Name"
+                />
+                <label
+                  htmlFor="name"
+                  className="absolute left-3 -top-6 bg-transparent text-sm leading-7 text-gray-500 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:left-3 peer-focus:-top-6 peer-focus:text-sm peer-focus:text-indigo-500"
+                >
+                  Name
+                </label>
+              </div>
             </div>
-          ))}
-        </div>
-
-        <div className="w-full md:w-1/2">
-          <img
-            className="w-full h-[25rem] rounded-sm shadow-lg"
-            src="/images/c2.jpg"
-            alt="Contact"
-          />
+            <div className="w-1/2 p-2">
+              <div className="relative">
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="peer w-full rounded border border-gray-700  bg-opacity-40 py-1 px-3 text-base leading-8 text-black placeholder-transparent outline-none transition-colors duration-200 ease-in-out focus:border-none focus:ring-2 focus:ring-indigo-900"
+                  placeholder="Email"
+                />
+                <label
+                  htmlFor="email"
+                  className="absolute left-3 -top-6 bg-transparent text-sm leading-7 text-gray-500 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:left-3 peer-focus:-top-6 peer-focus:text-sm peer-focus:text-gray-400"
+                >
+                  E-mail
+                </label>
+              </div>
+            </div>
+            <div className="mt-4 w-full p-2">
+              <div className="relative">
+                <textarea
+                  id="message"
+                  name="message"
+                  className="peer h-32 w-full resize-none rounded border border-gray-700  bg-opacity-40 py-1 px-3 text-base leading-6 text-black placeholder-transparent outline-none transition-colors duration-200 ease-in-out focus:border-none focus:ring-2 focus:ring-indigo-900"
+                  placeholder="Message"
+                ></textarea>
+                <label
+                  htmlFor="message"
+                  className="absolute left-3 -top-6 bg-transparent text-sm leading-7 text-gray-500 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:left-3 peer-focus:-top-6 peer-focus:text-sm peer-focus:text-gray-500"
+                >
+                  Message
+                </label>
+              </div>
+            </div>
+            <div className="w-full p-2">
+              <button className=" tracking-wider mx-auto flex rounded border-0 bg-orange-400 py-2 px-8 text-lg text-white hover:bg-orange-500 focus:outline-none items-center justify-center w-full">
+                SEND MESSAGE
+              </button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
