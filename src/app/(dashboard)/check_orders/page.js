aab85e242201a0c83e1c74b1738b96d9e1342146
Mahbub2001 <imahbubturza@gmail.com>
2025-01-03 19:04:31 +0600

    checkout

--- src/app/(dashboard)/check_orders/page.js
+++ src/app/(dashboard)/check_orders/page.js
@@ -224,13 +224,47 @@ function CheckOrders() {
                         </dd>
                       </dl>
                     )}
-
+                    {order?.status === "received" && (
+                      <dl className="w-1/2 sm:w-1/4 lg:w-auto lg:flex-1">
+                        <dt className="text-base font-medium text-gray-500 dark:text-gray-400">
+                          Status:
+                        </dt>
+                        <dd className="me-2 mt-1.5 inline-flex items-center rounded bg-blue-100 px-2.5 py-0.5 text-xs font-medium text-blue-800 dark:bg-blue-900 dark:text-blue-300">
+                          <svg
+                            className="me-1 h-3 w-3"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M6 18 17.94 6M18 18 6.06 6"
+                            />
+                          </svg>
+                          Received
+                        </dd>
+                      </dl>
+                    )}
                     <div className="w-full grid sm:grid-cols-2 lg:flex lg:w-64 lg:items-center lg:justify-end gap-4">
+                      {order?.status === "received" && (
+                        <button
+                          type="button"
+                          className="w-full rounded-lg bg-primary-700 px-3 py-2 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 lg:w-auto"
+                        >
+                          Give Review
+                        </button>
+                      )}
                       <button
                         type="button"
                         className="w-full rounded-lg bg-primary-700 px-3 py-2 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 lg:w-auto"
                       >
-                        Order again
+                        Cancel
                       </button>
                       <button
                         onClick={(e) => {
