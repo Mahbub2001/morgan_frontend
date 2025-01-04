2c1feb53da63e3d755efe5ec8b578f92fa97fccb
Mahbub2001 <imahbubturza@gmail.com>
2025-01-05 01:45:56 +0600

    update_settings

--- src/app/(dashboard)/admin_roles/page.js
+++ src/app/(dashboard)/admin_roles/page.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import Button3 from "@/containers/common/Button3/Button3";
+import React from "react";
 
 function AdminRoles() {
   return (
     <div>
-      this is admin roles page still working on it
+      <h1 className="text-center font-thin text-xl">Some Settings</h1>
+      <div className="mt-10">
+        <div className="flex flex-col md:flex-row gap-5 items-center">
+          <div>
+            <label
+              htmlFor="firstTop"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              First Top
+            </label>
+            <textarea
+              type="text"
+              id="firstTop"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block h-[10rem]  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Discount Offer"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="firstTop"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Second Top
+            </label>
+            <textarea
+              type="text"
+              id="firstTop"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block h-[10rem]  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Free Shipping Offer"
+              required
+            />
+          </div>
+          <div>
+            <Button3
+              text="SAVE"
+              textColor="white"
+              backgroundColor="orange"
+              borderColor="orange"
+            />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AdminRoles
+export default AdminRoles;
