cc8571393f2213dcc2a57bcd8b0fb70aa2810189
Mahbub2001 <imahbubturza@gmail.com>
2025-01-12 14:43:47 +0600

    pie-chart-sales

--- src/components/ChartsComponent/ChartsComponent.js
+++ src/components/ChartsComponent/ChartsComponent.js
@@ -20,9 +20,26 @@ function ChartsComponent() {
       });
   }, []);
 
+  const [data2, setData2] = useState({
+    personData: [],
+    categoryData: [],
+    subCategoryData: [],
+  });
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_API_URL}/pie-chart-sales`)
+      .then((response) => {
+        setData2(response.data);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch chart data", error);
+      });
+  }, []);
+
   return (
     <div className="mt-10">
-      <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center">
         <div>
           <h3 className="my-8 text-center text-2xl">Products Distribution</h3>
           <div className="grid grid-cols-1 lg:grid-cols-2">
@@ -38,7 +55,20 @@ function ChartsComponent() {
           </div>
         </div>
         <div>
-          
+          <div>
+            <h3 className="my-8 text-center text-2xl">Products Sales Distribution</h3>
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+              <PieChart title="Persons Distribution" data={data2.personData} />
+              <PieChart
+                title="Categories Distribution"
+                data={data2.categoryData}
+              />
+              <PieChart
+                title="Subcategories Distribution"
+                data={data2.subCategoryData}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
